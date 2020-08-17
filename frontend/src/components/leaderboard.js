78131716918ac28ca55624014beb1b34f632d0d0
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-08-17 19:29:04 +0100

    Added empty leaderboard component

---
+++ frontend/src/components/leaderboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../App.css';
+import Banner from './banner';
+
+function Leaderboard() {
+  return (
+    <div className="Leaderboard">
+      <Banner />
+    </div>
+  );
+}
+
+export default Leaderboard;
