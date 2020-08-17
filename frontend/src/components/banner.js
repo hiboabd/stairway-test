b82a61f31238fdb24edfbaf42070b06b0cb017e9
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-08-17 18:29:08 +0100

    Added reset banner

--- frontend/src/components/banner.js
+++ frontend/src/components/banner.js
@@ -4,7 +4,10 @@ import '../App.css';
 function Banner() {
   return (
     <div className="Banner">
-      <h3> Compete with your friends this week! </h3>
+        <h3> Compete with your friends this week! </h3>
+        <div className="Reset">
+          <h4> Resets in: 4 days</h4> 
+        </div>
     </div>
   );
 }
