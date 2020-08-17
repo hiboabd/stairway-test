7cf836e6cf3e678f49c6126d65d36062a2efc741
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-08-17 19:28:20 +0100

    Added banner component

--- frontend/src/components/banner.js
+++ frontend/src/components/banner.js
@@ -4,9 +4,11 @@ import '../App.css';
 function Banner() {
   return (
     <div className="Banner">
-        <h3> Compete with your friends this week! </h3>
+        <div className="Banner-title">
+          <h3> Compete with your friends this week! </h3>
+        </div>
         <div className="Reset">
-          <h4> Resets in: 4 days</h4> 
+          <h4> Resets in: 4 days</h4>
         </div>
     </div>
   );
