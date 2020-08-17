bfb73f71f9da954af924acb3fe769d31627e351c
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-08-17 18:28:52 +0100

    Added second test for banner reset text

--- frontend/src/banner.test.js
+++ frontend/src/banner.test.js
@@ -4,6 +4,13 @@ import Banner from './components/banner.js';
 
 test('renders the title for the banner', () => {
   const { getByText } = render(<Banner />);
-  const linkElement = getByText(/Compete with your friends this week!/i);
-  expect(linkElement).toBeInTheDocument();
+  const textElement = getByText(/Compete with your friends this week!/i);
+  expect(textElement).toBeInTheDocument();
+});
+
+
+test('renders the time left before leaderboard reset', () => {
+  const { getByText } = render(<Banner />);
+  const textElement = getByText(/Resets in: 4 days/i);
+  expect(textElement).toBeInTheDocument();
 });
