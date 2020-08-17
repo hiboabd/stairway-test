7808c5da6953a8d2f83631a6cdda77d2e6dc40a3
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-08-17 16:21:57 +0100

    Added banner component

---
+++ frontend/src/components/banner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../App.css';
+
+function Banner() {
+  return (
+    <div className="Banner">
+      <h3> Compete with your friends this week! </h3>
+    </div>
+  );
+}
+
+export default Banner;
