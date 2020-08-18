47d081b28701a53328291a4bbc9366da1f4e712e
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-08-18 09:36:47 +0100

    Modified banner tests due to text modification

--- frontend/src/components/__tests__/banner.test.js
+++ frontend/src/components/__tests__/banner.test.js
@@ -9,8 +9,10 @@ test('renders the title for the banner', () => {
 });
 
 
-test('renders the time left before leaderboard reset', () => {
-  const { getByText } = render(<Banner />);
-  const textElement = getByText(/Resets in: 4 days/i);
-  expect(textElement).toBeInTheDocument();
+test('renders the reset information', () => {
+  const { container } = render(<Banner />);
+  const container1 = container.querySelector('.reset-text')
+  const container2 = container.querySelector('.reset-time')
+  expect(container1).toBeInTheDocument();
+  expect(container2).toBeInTheDocument();
 });
