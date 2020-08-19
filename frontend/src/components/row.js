26e88b6e5b67ee5ea69719b602db889dec44d8ee
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-08-19 14:57:15 +0100

    Improved formatting

--- frontend/src/components/row.js
+++ frontend/src/components/row.js
@@ -3,10 +3,14 @@ import '../App.css';
 
 function Row(user, index) {
   return (
-    <div className="row" data-testid="users" style={{ background: user.user.isCurrentUser ? '#F2F5FC' : '#FFFFFF' }}>
-      <h4 className="index"> {user.index + 1} </h4>
-      <h4 className="name"> {user.user.username} </h4>
-      <h4 className="points"> {user.user.weeklyXP}XP  </h4>
+    <div
+      className="row"
+      data-testid="users"
+      style={{ background: user.user.isCurrentUser ? '#F2F5FC' : '#FFFFFF' }}>
+
+        <h4 className="index"> {user.index + 1} </h4>
+        <h4 className="name"> {user.user.username} </h4>
+        <h4 className="points"> {user.user.weeklyXP}XP </h4>
     </div>
   );
 }
