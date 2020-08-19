b561756d4dc24ea4fcf0d376ae36735cea09af03
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-08-19 13:28:50 +0100

    Implemented conditional background rendering

--- frontend/src/components/row.js
+++ frontend/src/components/row.js
@@ -3,7 +3,7 @@ import '../App.css';
 
 function Row(user, index) {
   return (
-    <div className="row" data-testid="users">
+    <div className="row" data-testid="users" style={{ background: user.user.isCurrentUser ? '#F2F5FC' : '#FFFFFF' }}>
       <h4 className="index"> {user.index + 1} </h4>
       <h4 className="name"> {user.user.username} </h4>
       <h4 className="points"> {user.user.weeklyXP}XP  </h4>
